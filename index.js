d63db9a2691f0a2c6b9a6c97f4527abe5aaddcf0
sabbir <sabbir21122@gmail.com>
2022-07-17 12:11:16 +0600

    redux core

---
+++ index.js
@@ -0,0 +1,55 @@
+const {createStore}=require("redux")
+
+
+
+const increment="increment"
+const decrement="decrement"
+//initial state
+const initialState = {
+    count: 0
+}
+//action function
+const incrementCounterAction=()=>{
+    return{
+        type:increment
+    }
+}
+const decrementCounterAction=()=>{
+    return{
+        type:decrement
+    }
+}
+
+
+//Reducer
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case increment:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case decrement:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+
+        default:
+            return state;
+    }
+
+}
+
+
+
+const store = createStore(counterReducer)
+
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+store.dispatch(incrementCounterAction())
+store.dispatch(incrementCounterAction())
+store.dispatch(incrementCounterAction())
+store.dispatch(incrementCounterAction())
+store.dispatch(incrementCounterAction())
